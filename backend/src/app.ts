aa6a972b0aa5d8998f1dee6793498400998773e2
alfarorangkuti-lang <alfarorangkuti@gmail.com>
2026-05-18 01:40:28 +0700

    generate JWT

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,7 @@ import pool from "./config/db";
 import categoriesRoutes from "./routes/categoriesRoutes";
 import productsRoutes from "./routes/productsRoutes";
 import transactionRoutes from "./routes/transactionRoute"
+import authRoutes from "./routes/authRoutes"
 
 const app = express();
 
@@ -15,7 +16,7 @@ app.use(express.json());
 app.use('/products', productsRoutes)
 app.use("/categories", categoriesRoutes);
 app.use("/transactions", transactionRoutes)
-
+app.use("/auth", authRoutes)
 
 app.get("/", (_, res) => {
   res.status(200).json("Practine POS API");
---
+++ backend/src/controllers/authController.ts
@@ -0,0 +1,36 @@
+import bcrypt from "bcryptjs"
+import { Request, Response } from "express"
+import jwt from "jsonwebtoken"
+import { RowDataPacket } from "mysql2"
+import pool from "../config/db"
+
+interface User extends RowDataPacket {
+    id: number
+    username: string
+    password: string
+}
+
+export const login = async(req: Request, res: Response) => {
+    try {
+        
+    const {username, password} = req.body
+    const [result] = await pool.query<User[]>("SELECT * FROM users WHERE username = ?", [username])
+    const user = result[0]
+    
+    if(!user){
+        return res.status(404).json({message:"user tidak ditemukan!"})
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) {
+        return res.status(400).json({message: "Password salah!"})
+    }
+
+    const token = jwt.sign({id: user.id, email: user.username}, process.env.JWT_SECRET as string, {expiresIn: "1d"})
+    res.json({token})
+    } catch (error) {
+        res.status(500).json(
+            {message: `terjadi error : ${error}`}
+        )
+    }
+}
